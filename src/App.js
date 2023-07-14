fdba94efd543d29a806684d53e6475152f8e861b
Blessings Chinyanta <108142464+Blessings4321@users.noreply.github.com>
2023-07-14 12:57:29 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import  Axios  from 'axios';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [catfact, setCatFact] = useState("");
+
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+    setCatFact(res.data.fact);
+  }); 
+  } 
+
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+
+  
+    return (
+      <div className="App">
+        <button onClick={fetchCatFact}> Generate Fact About Cats</button>
+        <p> {catfact} </p>
+        
+      </div>
+    );
+  }
+
+export default App;
